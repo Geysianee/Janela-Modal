9835dff33e2bf79e9f889c49ebf4e9f18ec0559a
Geysiane Ramos <rgeisinha@gmail.com>
2024-08-25 17:59:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirModal() {
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+            modal.classList.remove('abrir')
+        }
+        
+    })
+}
